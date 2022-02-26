bf82bab65547f9858f38924f84ba60f6e3f3f782
Markus Doits <markus.doits@stellenticket.de>
2022-02-26 17:31:43 +0100

    make `with_all_rich_text` work with translated rich texts

--- test_app/db/migrate/20210114124951_translate_rich_texts.rb
+++ test_app/db/migrate/20210114124951_translate_rich_texts.rb
@@ -3,7 +3,7 @@
 class TranslateRichTexts < ActiveRecord::Migration[6.1]
   def change
     # or null: true to allow untranslated rich text
-    add_column :action_text_rich_texts, :locale, :string, null: false
+    add_column :action_text_rich_texts, :locale, :string, null: true
 
     remove_index :action_text_rich_texts,
                  column: %i[record_type record_id name],
